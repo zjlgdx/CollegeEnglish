138e2502b1c3b0cff3c65aa74e36ca0184e8a51e
yvan <zjlgdx@gmail.com>
2014-09-18 22:52:07 +0800

    update to publish to store

---
+++ CollegeEnglish/DataModel/JsonStringExtension.cs
@@ -0,0 +1,22 @@
+﻿using Windows.Data.Json;
+
+namespace CollegeEnglish.DataModel
+{
+    public static class JsonStringExtension
+    {
+        public static string ToJsonString(this IJsonValue obj, string prefix = null)
+        {
+            if (obj == null || obj.ValueType == JsonValueType.Null)
+            {
+                return string.Empty;
+            }
+
+            if (!string.IsNullOrEmpty(prefix))
+            {
+                return prefix + obj.GetString();
+            }
+
+            return obj.GetString();
+        }
+    }
+}
